55f3bff99c35a6169899e0c943ce2d69afbe3d63
Muhammad Alam <alam@mmtutor.com>
2017-03-23 18:05:28 -0400

    added and migrated bands, tracks, and albums table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323153414) do
+ActiveRecord::Schema.define(version: 20170323214140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "name",                       null: false
+    t.integer  "band_id",                    null: false
+    t.integer  "year",                       null: false
+    t.boolean  "live",       default: false, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["band_id", "name"], name: "index_albums_on_band_id_and_name", unique: true, using: :btree
+  end
+
+  create_table "bands", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.integer  "album_id",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_tracks_on_album_id", unique: true, using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
